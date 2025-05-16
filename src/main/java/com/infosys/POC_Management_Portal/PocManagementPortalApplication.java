11f48a897acfa81eca01d73cd5819f876548ea40
harshanricse <tunuguntlaharsha@gmail.com>
2025-05-16 17:37:49 +0530

    first commit

---
+++ src/main/java/com/infosys/POC_Management_Portal/PocManagementPortalApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.POC_Management_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocManagementPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocManagementPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosys/POC_Management_Portal/PocManagementPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.POC_Management_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocManagementPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
